6c1e8e2040dbc3320901fbabd0bcf329de43e4e5
Denis Michailov <kamrad.reg@gmail.com>
2017-09-27 18:33:30 +1000

    loader: working on

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,23 @@
 // Libraries
-
 let $ = require('jquery');
 window.$ = $;
 window.jquery = $;
 window.jQuery = $;
 
-// let Tether = require('../../node_modules/tether/dist/js/tether');
-// window.Tether = Tether;
-// require('../../node_modules/bootstrap/dist/js/bootstrap');
-// let Handlebars = require('handlebars');
 window.onload = function() {
   console.log("--- Loaded ---");
-  function second_passed() {
-    let clock = document.querySelector('.clock');
-    clock.classList.remove('is-off');
-  }
 
-  setTimeout(second_passed, 4000);
+  let message = $('.message');
+  message.text('Done!');
+  message.attr('data-message', 'Done!')
+
+  setTimeout(function() {
+
+    let appContent = document.querySelector('.app-content');
+    let loader = document.querySelector('.loader');
+    appContent.removeChild(loader);
+    appContent.classList.remove('overlap');
+
+  }, 5000);
 
 }
-
-console.log("Basic Project");
-
-let importedModule = require('./module');
-console.dir( importedModule );
